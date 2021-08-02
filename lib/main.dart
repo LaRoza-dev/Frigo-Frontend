c3e2221e4f9ea1710443cf8642f4c8fcd274d644
Sepehr Ghafari <sphr.ghfri@gmail.com>
2021-08-02 11:17:38 +0430

    first page PNG positioned fixed and is responsive

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    systemNavigationBarColor: Color(0xFF000000),
+    statusBarColor: Colors.transparent,
+    systemNavigationBarIconBrightness: Brightness.light,
+    statusBarIconBrightness: Brightness.dark,
+    statusBarBrightness: Brightness.light,
+  ));
   runApp(Main());
 }
 
@@ -9,42 +18,59 @@ class Main extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: new Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            // new Stack(children: <Widget>[
-            Expanded(
-              flex: 2,
-              child: new Container(
-                color: Colors.white,
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(50, 100, 50, 0),
-                  child: new Text(
-                    'All foods that you can easily in a single app.',
-                    style: TextStyle(
-                        fontSize: 30,
-                        color: Color(0xff0D1863),
-                        fontWeight: FontWeight.w400),
+    return MaterialApp(home: Home());
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Stack(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                flex: 2,
+                child: new Container(
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(50, 100, 50, 0),
+                    child: new Text(
+                      'All foods that you can easily in a single app.',
+                      style: TextStyle(
+                          fontSize: 30,
+                          color: Color(0xff0D1863),
+                          fontWeight: FontWeight.w400,
+                          fontFamily: 'Poppins'),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Expanded(
-              flex: 1,
-              child: new Container(
-                // width: ,
-                // height: 80.0,
-                decoration: new BoxDecoration(
-                  color: Color(0xffb74093),
+              Expanded(
+                flex: 1,
+                child: new Container(
+                  decoration:
+                      new BoxDecoration(color: Color.fromRGBO(160, 77, 255, 1)),
                 ),
               ),
+            ],
+          ),
+          Positioned(
+            top: height*0.25,
+            left: width*0.1,
+            right: width*0.1,
+            child: SvgPicture.asset(
+              'asset/images/test.svg',height: height*0.55,width: width*0.5,
             ),
-            // ])
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }

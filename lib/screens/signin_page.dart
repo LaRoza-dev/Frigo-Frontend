d26a7a113ba3eed8e712b5606f1489bbf529c52a
Sepehr Ghafari <sphr.ghfri@gmail.com>
2021-08-12 21:08:21 +0430

    open keyboard scroll in signup and signin pages fixed

--- lib/screens/signin_page.dart
+++ lib/screens/signin_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fridge/constants.dart';
 import 'package:fridge/components/buttons.dart';
 import 'package:fridge/components/textFields.dart';
+import 'package:fridge/components/modal_profile.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -13,97 +14,112 @@ class SignInPage extends StatelessWidget {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       backgroundColor: kBackgroundColor,
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Row(
+        child: CustomScrollView(
+          scrollDirection: Axis.vertical,
+          slivers: [
+            SliverFillRemaining(
+              hasScrollBody: false,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Expanded(
-                    flex: 1,
-                    child: IconButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        icon: FaIcon(FontAwesomeIcons.chevronLeft)),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: IconButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              icon: FaIcon(FontAwesomeIcons.chevronLeft)),
+                        ),
+                        Expanded(
+                          flex: 8,
+                          child: Text(
+                            'Sign In',
+                            style: kHeading1,
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: SizedBox(),
+                        )
+                      ],
+                    ),
                   ),
                   Expanded(
-                    flex: 9,
+                    child: SvgPicture.asset(
+                      'asset/images/SignInPageImage.svg',
+                      alignment: Alignment.center,
+                    ),
+                  ),
+                  InsertTextFlied(
+                    width: width,
+                    text: 'User name',
+                  ),
+                  InsertTextFlied(
+                    width: width,
+                    text: 'Password',
+                    textAction: TextInputAction.done,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: MainButton(
+                      onTap: () {
+                        profileModal(context);
+                      },
+                      buttonTitle: 'Sign in',
+                      fontColor: kTextColor2,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
                     child: Text(
-                      'Sign In',
-                      style: kHeading1,
-                      textAlign: TextAlign.center,
+                      'Forgot your password?',
+                      style: ktext2.copyWith(
+                        shadows: [
+                          Shadow(
+                            offset: Offset(3.0, 6.0),
+                            blurRadius: 6.0,
+                            color: Colors.black.withOpacity(0.25),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                  Expanded(
-                    flex: 1,
-                    child: SizedBox(),
-                  )
+                  Container(
+                    width: 150,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: FaIcon(FontAwesomeIcons.facebook),
+                          iconSize: 45,
+                          color: Color.fromRGBO(13, 24, 100, 0.9),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: FaIcon(FontAwesomeIcons.googlePlus),
+                          iconSize: 45,
+                          color: Color.fromRGBO(13, 24, 100, 0.9),
+                        ),
+                        SizedBox(
+                          height: height * 0.02,
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: height * 0.02),
                 ],
               ),
             ),
-            Expanded(
-              child: SvgPicture.asset(
-                'asset/images/SignInPageImage.svg',
-                alignment: Alignment.center,
-              ),
-            ),
-            InsertTextFlied(
-              width: width,
-              text: 'User name',
-            ),
-            InsertTextFlied(
-              width: width,
-              text: 'Password',
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: MainButton(
-                onTap: () {},
-                buttonTitle: 'Sign in',
-                fontColor: kTextColor2,
-              ),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text(
-                'Forgot your password?',
-                style: ktext2.copyWith(
-                  shadows: [
-                    Shadow(
-                      offset: Offset(3.0, 6.0),
-                      blurRadius: 6.0,
-                      color: Colors.black.withOpacity(0.25),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Container(width: 150,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: FaIcon(FontAwesomeIcons.facebook),
-                    iconSize: 45,
-                    color: Color.fromRGBO(13, 24, 100, 0.9),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: FaIcon(FontAwesomeIcons.googlePlus),
-                    iconSize: 45,
-                    color: Color.fromRGBO(13, 24, 100, 0.9),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: height * 0.07),
           ],
         ),
       ),
--- lib/screens/signup_page.dart
+++ lib/screens/signup_page.dart
@@ -13,103 +13,111 @@ class SignUpPage extends StatelessWidget {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       backgroundColor: kBackgroundColor,
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Row(
+        child: CustomScrollView(
+          scrollDirection: Axis.vertical,
+          slivers: [
+            SliverFillRemaining(
+              hasScrollBody: false,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Expanded(
-                    flex: 1,
-                    child: IconButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        icon: FaIcon(FontAwesomeIcons.chevronLeft)),
-                  ),
-                  Expanded(
-                    flex: 9,
-                    child: Text(
-                      'Sign Up',
-                      style: kHeading1,
-                      textAlign: TextAlign.center,
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: IconButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              icon: FaIcon(FontAwesomeIcons.chevronLeft)),
+                        ),
+                        Expanded(
+                          flex: 8,
+                          child: Text(
+                            'Sign Up',
+                            style: kHeading1,
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: SizedBox(),
+                        )
+                      ],
                     ),
                   ),
                   Expanded(
-                    flex: 1,
-                    child: SizedBox(),
-                  )
+                    child: SvgPicture.asset(
+                      'asset/images/SignUpPageImage.svg',
+                      alignment: Alignment.center,
+                    ),
+                  ),
+                  InsertTextFlied(
+                    width: width,
+                    text: 'User name',
+                  ),
+                  InsertTextFlied(
+                    width: width,
+                    text: 'Full name',
+                  ),
+                  InsertTextFlied(
+                    width: width,
+                    text: 'Password',
+                  ),
+                  InsertTextFlied(
+                    width: width,
+                    text: 'Repeat Password',
+                    textAction: TextInputAction.done,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: MainButton(
+                      onTap: () {},
+                      buttonTitle: 'Sign up',
+                      fontColor: kTextColor2,
+                    ),
+                  ),
+                  Text(
+                    'OR',
+                    style: ktext2.copyWith(
+                      shadows: [
+                        Shadow(
+                          offset: Offset(3.0, 6.0),
+                          blurRadius: 6.0,
+                          color: Colors.black.withOpacity(0.25),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: 150,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: FaIcon(FontAwesomeIcons.facebook),
+                          iconSize: 45,
+                          color: Color.fromRGBO(13, 24, 100, 0.9),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: FaIcon(FontAwesomeIcons.googlePlus),
+                          iconSize: 45,
+                          color: Color.fromRGBO(13, 24, 100, 0.9),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: height * 0.07),
                 ],
               ),
             ),
-            Expanded(
-              child: SvgPicture.asset(
-                'asset/images/SignUpPageImage.svg',
-                alignment: Alignment.center,
-              ),
-            ),
-            InsertTextFlied(
-              width: width,
-              text: 'User name',
-            ),
-            InsertTextFlied(
-              width: width,
-              text: 'Full name',
-            ),
-            InsertTextFlied(
-              width: width,
-              text: 'Password',
-            ),
-            InsertTextFlied(
-              width: width,
-              text: 'Repeat Password',
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: MainButton(
-                onTap: () {},
-                buttonTitle: 'Sign up',
-                fontColor: kTextColor2,
-              ),
-            ),
-            Text(
-              'OR',
-              style: ktext2.copyWith(
-                shadows: [
-                  Shadow(
-                    offset: Offset(3.0, 6.0),
-                    blurRadius: 6.0,
-                    color: Colors.black.withOpacity(0.25),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              width: 150,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: FaIcon(FontAwesomeIcons.facebook),
-                    iconSize: 45,
-                    color: Color.fromRGBO(13, 24, 100, 0.9),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: FaIcon(FontAwesomeIcons.googlePlus),
-                    iconSize: 45,
-                    color: Color.fromRGBO(13, 24, 100, 0.9),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: height * 0.07),
           ],
         ),
       ),

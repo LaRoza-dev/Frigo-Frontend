d840120f9c982d8330ca7f650e79ac3311a525cc
Roshan <roshanghojoghi@gmail.com>
2021-08-21 15:02:07 +0300

    Sign in page bug fixed
    Sign in page bug fixed

--- lib/controllers/form_controller.dart
+++ lib/controllers/form_controller.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:form_field_validator/form_field_validator.dart';
 
 class FormController extends GetxController {
-  final GlobalKey<FormState> loginFormKey = GlobalKey<FormState>();
+  GlobalKey<FormState> loginFormKey = GlobalKey<FormState>();
   late TextEditingController emailController,
       passwordController,
       passowrdRepeatController;
@@ -49,11 +49,8 @@ class FormController extends GetxController {
     final isValid = loginFormKey.currentState!.validate();
     if (!isValid) {
       return false;
-    }
-    loginFormKey.currentState!.save();
-    if (loginFormKey.isBlank!) {
-      return false;
     } else {
+      loginFormKey.currentState!.save();
       return true;
     }
   }

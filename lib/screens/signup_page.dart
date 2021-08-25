6e1fd5e300975ffa2d2fac37dd0fac62090b0e80
Roshan <roshanghojoghi@gmail.com>
2021-08-26 02:47:29 +0300

    Update signup_page.dart
    sign up page scroll view fixed

--- lib/screens/signup_page.dart
+++ lib/screens/signup_page.dart
@@ -24,218 +24,217 @@ class SignUpPage extends GetView {
     return Scaffold(
       backgroundColor: kBackgroundColor,
       body: SafeArea(
-        child: CustomScrollView(
-          scrollDirection: Axis.vertical,
-          slivers: [
-            SliverFillRemaining(
-              hasScrollBody: false,
-              child: Form(
-                //autovalidateMode: AutovalidateMode.always,
-                key: formController.loginFormKey,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            flex: 1,
-                            child: IconButton(
-                                onPressed: () {
-                                  Get.offAllNamed('/');
-                                },
-                                icon: FaIcon(FontAwesomeIcons.chevronLeft)),
+        child: SingleChildScrollView(
+          reverse: true,
+          child: Container(
+            height: height,
+            width: width,
+            child: Form(
+              //autovalidateMode: AutovalidateMode.always,
+              key: formController.loginFormKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: IconButton(
+                              onPressed: () {
+                                Get.offAllNamed('/');
+                              },
+                              icon: FaIcon(FontAwesomeIcons.chevronLeft)),
+                        ),
+                        Expanded(
+                          flex: 8,
+                          child: Text(
+                            'Sign Up',
+                            style: kHeading1,
+                            textAlign: TextAlign.center,
                           ),
-                          Expanded(
-                            flex: 8,
-                            child: Text(
-                              'Sign Up',
-                              style: kHeading1,
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                          Expanded(
-                            flex: 1,
-                            child: SizedBox(),
-                          )
-                        ],
-                      ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: SizedBox(),
+                        )
+                      ],
                     ),
-                    Expanded(
-                      child: SvgPicture.asset(
-                        'asset/images/SignUpPageImage.svg',
-                        alignment: Alignment.center,
-                      ),
+                  ),
+                  Expanded(
+                    child: SvgPicture.asset(
+                      'asset/images/SignUpPageImage.svg',
+                      alignment: Alignment.center,
                     ),
-                    GetBuilder<UserController>(builder: (_) {
-                      return InsertTextFlied(
-                          width: width,
-                          text: 'User name',
-                          validator: formController.emailValidator,
-                          controller: formController.emailController,
-                          onSaved: (value) {
-                            formController.email = value;
-                          },
-                          onChanged: (username) {
-                            _.setEmail(username);
-                          });
-                    }),
-                    GetBuilder<UserController>(builder: (_) {
-                      return InsertTextFlied(
+                  ),
+                  GetBuilder<UserController>(builder: (_) {
+                    return InsertTextFlied(
                         width: width,
-                        text: 'Full name',
-                        validator: formController.requiredValidator,
-                        onChanged: (fullName) {
-                          _.setFullname(fullName);
-                        },
-                      );
-                    }),
-                    GetBuilder<UserController>(builder: (_) {
-                      return InsertTextFlied(
-                        width: width,
-                        text: 'Password',
-                        validator: formController.passwordValidator,
-                        controller: formController.passwordController,
+                        text: 'User name',
+                        validator: formController.emailValidator,
+                        controller: formController.emailController,
                         onSaved: (value) {
-                          formController.password = value;
+                          formController.email = value;
                         },
-                        obscureText: true,
-                        onChanged: (password) {
-                          formController.password = password;
-                          _.setPassword(password);
-                        },
-                      );
-                    }),
-                    InsertTextFlied(
+                        onChanged: (username) {
+                          _.setEmail(username);
+                        });
+                  }),
+                  GetBuilder<UserController>(builder: (_) {
+                    return InsertTextFlied(
                       width: width,
-                      text: 'Repeat Password',
-                      textAction: TextInputAction.done,
-                      controller: formController.passowrdRepeatController,
-                      onSaved: (value) {
-                        formController.passwordRepeat = value;
+                      text: 'Full name',
+                      validator: formController.requiredValidator,
+                      onChanged: (fullName) {
+                        _.setFullname(fullName);
                       },
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'This field is required';
-                        }
-                        if (value != formController.password) {
-                          return 'Confimation password does not match the entered password';
-                        }
+                    );
+                  }),
+                  GetBuilder<UserController>(builder: (_) {
+                    return InsertTextFlied(
+                      width: width,
+                      text: 'Password',
+                      validator: formController.passwordValidator,
+                      controller: formController.passwordController,
+                      onSaved: (value) {
+                        formController.password = value;
                       },
                       obscureText: true,
-                    ),
-                    Padding(
-                        padding: const EdgeInsets.only(bottom: 8, top: 20),
-                        child: GetBuilder<UserController>(
-                          builder: (_) {
-                            return MainButton(
-                              onPressed: () async {
-                                if (formController.checkLogin()) {
-                                  bool res = await _.signUp();
-                                  if (res) {
-                                    Alert(
-                                      context: context,
-                                      type: AlertType.success,
-                                      title: "Register Successful",
-                                      desc: "Now you can login.",
-                                      buttons: [
-                                        DialogButton(
-                                          child: Text(
-                                            "Ok",
-                                            style: TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 20),
-                                          ),
-                                          onPressed: () {
-                                            Get.offAllNamed('/');
-                                          },
-                                          width: 120,
-                                        )
-                                      ],
-                                    ).show();
-                                  } else {
-                                    Alert(
-                                      context: context,
-                                      type: AlertType.error,
-                                      title: "Permission Denied",
-                                      desc:
-                                          "Email already exists or some error occured.",
-                                      buttons: [
-                                        DialogButton(
-                                          child: Text(
-                                            "Ok",
-                                            style: TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 20),
-                                          ),
-                                          onPressed: () {
-                                            Get.back();
-                                          },
-                                          width: 120,
-                                        )
-                                      ],
-                                    ).show();
-                                  }
+                      onChanged: (password) {
+                        formController.password = password;
+                        _.setPassword(password);
+                      },
+                    );
+                  }),
+                  InsertTextFlied(
+                    width: width,
+                    text: 'Repeat Password',
+                    textAction: TextInputAction.done,
+                    controller: formController.passowrdRepeatController,
+                    onSaved: (value) {
+                      formController.passwordRepeat = value;
+                    },
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'This field is required';
+                      }
+                      if (value != formController.password) {
+                        return 'Confimation password does not match the entered password';
+                      }
+                    },
+                    obscureText: true,
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.only(bottom: 8, top: 20),
+                      child: GetBuilder<UserController>(
+                        builder: (_) {
+                          return MainButton(
+                            onPressed: () async {
+                              if (formController.checkLogin()) {
+                                bool res = await _.signUp();
+                                if (res) {
+                                  Alert(
+                                    context: context,
+                                    type: AlertType.success,
+                                    title: "Register Successful",
+                                    desc: "Now you can login.",
+                                    buttons: [
+                                      DialogButton(
+                                        child: Text(
+                                          "Ok",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 20),
+                                        ),
+                                        onPressed: () {
+                                          Get.offAllNamed('/');
+                                        },
+                                        width: 120,
+                                      )
+                                    ],
+                                  ).show();
+                                } else {
+                                  Alert(
+                                    context: context,
+                                    type: AlertType.error,
+                                    title: "Permission Denied",
+                                    desc:
+                                        "Email already exists or some error occured.",
+                                    buttons: [
+                                      DialogButton(
+                                        child: Text(
+                                          "Ok",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 20),
+                                        ),
+                                        onPressed: () {
+                                          Get.back();
+                                        },
+                                        width: 120,
+                                      )
+                                    ],
+                                  ).show();
                                 }
-                              },
-                              buttonTitle: 'Sign up',
-                              fontColor: kTextColor2,
-                            );
-                          },
-                        )),
-                    Text(
-                      'OR',
-                      style: ktext2.copyWith(
-                        shadows: [
-                          Shadow(
-                            offset: Offset(3.0, 6.0),
-                            blurRadius: 6.0,
-                            color: Colors.black.withOpacity(0.25),
-                          ),
-                        ],
-                      ),
+                              }
+                            },
+                            buttonTitle: 'Sign up',
+                            fontColor: kTextColor2,
+                          );
+                        },
+                      )),
+                  Text(
+                    'OR',
+                    style: ktext2.copyWith(
+                      shadows: [
+                        Shadow(
+                          offset: Offset(3.0, 6.0),
+                          blurRadius: 6.0,
+                          color: Colors.black.withOpacity(0.25),
+                        ),
+                      ],
                     ),
-                    GetBuilder<UserController>(builder: (_) {
-                      return GoogleButton(
-                          text: 'Sign in with Google',
-                          onPressed: () async {
-                            googleLoginStatus? res = await _.googleLogin();
-                            print({"res": res});
-                            if (res == googleLoginStatus.error) {
-                              Alert(
-                                context: context,
-                                type: AlertType.error,
-                                title: "Permission Denied",
-                                desc: "Username or Password is incorrect.",
-                                buttons: [
-                                  DialogButton(
-                                    child: Text(
-                                      "Ok",
-                                      style: TextStyle(
-                                          color: Colors.white, fontSize: 20),
-                                    ),
-                                    onPressed: () {
-                                      Get.back();
-                                    },
-                                    width: 120,
-                                  )
-                                ],
-                              ).show();
-                            } else if (res == googleLoginStatus.loggedIn) {
-                              Get.toNamed('/home');
-                            } else {
-                              return Future.value();
-                            }
-                          });
-                    }),
-                    SizedBox(height: height * 0.07),
-                  ],
-                ),
+                  ),
+                  GetBuilder<UserController>(builder: (_) {
+                    return GoogleButton(
+                        text: 'Sign in with Google',
+                        onPressed: () async {
+                          googleLoginStatus? res = await _.googleLogin();
+                          print({"res": res});
+                          if (res == googleLoginStatus.error) {
+                            Alert(
+                              context: context,
+                              type: AlertType.error,
+                              title: "Permission Denied",
+                              desc: "Username or Password is incorrect.",
+                              buttons: [
+                                DialogButton(
+                                  child: Text(
+                                    "Ok",
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 20),
+                                  ),
+                                  onPressed: () {
+                                    Get.back();
+                                  },
+                                  width: 120,
+                                )
+                              ],
+                            ).show();
+                          } else if (res == googleLoginStatus.loggedIn) {
+                            Get.toNamed('/home');
+                          } else {
+                            return Future.value();
+                          }
+                        });
+                  }),
+                  SizedBox(height: height * 0.07),
+                ],
               ),
             ),
-          ],
+          ),
         ),
       ),
     );

9eb3c19b11fdd0e5f198dab9faaa7e476b84f2a7
Sepehr Ghafari <sphr.ghfri@gmail.com>
2021-08-27 17:04:45 +0430

    signin layout responsive fix

--- lib/screens/signin_page.dart
+++ lib/screens/signin_page.dart
@@ -21,20 +21,20 @@ class SignInPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: kBackgroundColor,
       body: SafeArea(
-        child: SingleChildScrollView(
+        child: SingleChildScrollView(padding: EdgeInsets.zero,
           reverse: true,
           child: Container(
-            height: height,
+            height: height-25,
             width: width,
             child: Form(
               key: formController.loginFormKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Row(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(10, 10, 10, 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Expanded(
@@ -59,136 +59,154 @@ class SignInPage extends StatelessWidget {
                         )
                       ],
                     ),
-                  ),
-                  Expanded(
-                    child: Image.asset(
-                      'asset/images/SignInPageImage.png',
-                      alignment: Alignment.center,
-                      fit: BoxFit.fitHeight,
-                    ),
-                  ),
-                  GetBuilder<UserController>(builder: (_) {
-                    return InsertTextFlied(
-                      width: width,
-                      text: 'Username',
-                      validator: formController.emailValidator,
-                      controller: formController.emailController,
-                      onSaved: (value) {
-                        formController.email = value;
-                        _.setEmail(value);
-                      },
-                    );
-                  }),
-                  GetBuilder<UserController>(builder: (_) {
-                    return InsertTextFlied(
-                      width: width,
-                      text: 'Password',
-                      textAction: TextInputAction.done,
-                      validator: formController.signupPasswordrequiredValidator,
-                      controller: formController.passwordController,
-                      onSaved: (value) {
-                        formController.password = value;
-                        _.setPassword(value);
-                      },
-                      obscureText: true,
-                    );
-                  }),
-                  GetBuilder<UserController>(builder: (_) {
-                    return MainButton(
-                      onPressed: () async {
-                        if (formController.checkLogin()) {
-                          bool res = await _.login();
-                          if (res) {
-                            Get.toNamed('/home');
-                          } else {
-                            Alert(
-                              context: context,
-                              type: AlertType.error,
-                              title: "Permission Denied",
-                              desc: "Username or Password is incorrect.",
-                              buttons: [
-                                DialogButton(
-                                  child: Text(
-                                    "Ok",
-                                    style: TextStyle(
-                                        color: Colors.white, fontSize: 20),
-                                  ),
-                                  onPressed: () {
-                                    Get.back();
-                                  },
-                                  width: 120,
-                                )
-                              ],
-                            ).show();
-                          }
-                        }
-                      },
-                      buttonTitle: 'Sign in',
-                      fontColor: kTextColor2,
-                    );
-                  }),
-                  TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      'Forgot your password?',
-                      style: ktext2.copyWith(
-                        shadows: [
-                          Shadow(
-                            offset: Offset(3.0, 6.0),
-                            blurRadius: 6.0,
-                            color: Colors.black.withOpacity(0.25),
-                          ),
-                        ],
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 25),
+                        child: Image.asset(
+                          'asset/images/SignInPageImage.png',
+                          alignment: Alignment.center,
+                          fit: BoxFit.fitWidth,
+                        ),
                       ),
                     ),
-                  ),
-                  Text(
-                    'OR',
-                    style: ktext2.copyWith(
-                      shadows: [
-                        Shadow(
-                          offset: Offset(3.0, 6.0),
-                          blurRadius: 6.0,
-                          color: Colors.black.withOpacity(0.25),
-                        ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        GetBuilder<UserController>(builder: (_) {
+                          return InsertTextFlied(
+                            width: width,
+                            text: 'Username',
+                            validator: formController.emailValidator,
+                            controller: formController.emailController,
+                            onSaved: (value) {
+                              formController.email = value;
+                              _.setEmail(value);
+                            },
+                          );
+                        }),
+                        GetBuilder<UserController>(builder: (_) {
+                          return InsertTextFlied(
+                            width: width,
+                            text: 'Password',
+                            textAction: TextInputAction.done,
+                            validator:
+                                formController.signupPasswordrequiredValidator,
+                            controller: formController.passwordController,
+                            onSaved: (value) {
+                              formController.password = value;
+                              _.setPassword(value);
+                            },
+                            obscureText: true,
+                          );
+                        }),
                       ],
                     ),
-                  ),
-                  GetBuilder<UserController>(builder: (_) {
-                    return GoogleButton(
-                        text: 'Sign in with Google',
-                        onPressed: () async {
-                          googleLoginStatus? res = await _.googleLogin();
-                          print({"res": res});
-                          if (res == googleLoginStatus.error) {
-                            Alert(
-                              context: context,
-                              type: AlertType.error,
-                              title: "Permission Denied",
-                              desc: "Username or Password is incorrect.",
-                              buttons: [
-                                DialogButton(
-                                  child: Text(
-                                    "Ok",
-                                    style: TextStyle(
-                                        color: Colors.white, fontSize: 20),
-                                  ),
-                                  onPressed: () {
-                                    Get.back();
-                                  },
-                                  width: 120,
-                                )
+                    Column(
+                      children: [
+                        GetBuilder<UserController>(builder: (_) {
+                          return Padding(
+                            padding: const EdgeInsets.only(top: 20),
+                            child: MainButton(
+                              onPressed: () async {
+                                if (formController.checkLogin()) {
+                                  bool res = await _.login();
+                                  if (res) {
+                                    Get.toNamed('/home');
+                                  } else {
+                                    Alert(
+                                      context: context,
+                                      type: AlertType.error,
+                                      title: "Permission Denied",
+                                      desc: "Username or Password is incorrect.",
+                                      buttons: [
+                                        DialogButton(
+                                          child: Text(
+                                            "Ok",
+                                            style: TextStyle(
+                                                color: Colors.white, fontSize: 20),
+                                          ),
+                                          onPressed: () {
+                                            Get.back();
+                                          },
+                                          width: 120,
+                                        )
+                                      ],
+                                    ).show();
+                                  }
+                                }
+                              },
+                              buttonTitle: 'Sign in',
+                              fontColor: kTextColor2,
+                            ),
+                          );
+                        }),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Forgot your password?',
+                            style: ktext2.copyWith(
+                              shadows: [
+                                Shadow(
+                                  offset: Offset(3.0, 6.0),
+                                  blurRadius: 6.0,
+                                  color: Colors.black.withOpacity(0.25),
+                                ),
                               ],
-                            ).show();
-                          } else if (res == googleLoginStatus.loggedIn) {
-                            Get.toNamed('/home');
-                          } else {
-                            return Future.value();
-                          }
-                        });
-                  }),
-                  SizedBox(height: height * 0.05),
-                ],
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                          child: Text(
+                            'OR',
+                            style: ktext2.copyWith(
+                              shadows: [
+                                Shadow(
+                                  offset: Offset(3.0, 6.0),
+                                  blurRadius: 6.0,
+                                  color: Colors.black.withOpacity(0.25),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        GetBuilder<UserController>(builder: (_) {
+                          return GoogleButton(
+                              text: 'Sign in with Google',
+                              onPressed: () async {
+                                googleLoginStatus? res = await _.googleLogin();
+                                print({"res": res});
+                                if (res == googleLoginStatus.error) {
+                                  Alert(
+                                    context: context,
+                                    type: AlertType.error,
+                                    title: "Permission Denied",
+                                    desc: "Username or Password is incorrect.",
+                                    buttons: [
+                                      DialogButton(
+                                        child: Text(
+                                          "Ok",
+                                          style: TextStyle(
+                                              color: Colors.white, fontSize: 20),
+                                        ),
+                                        onPressed: () {
+                                          Get.back();
+                                        },
+                                        width: 120,
+                                      )
+                                    ],
+                                  ).show();
+                                } else if (res == googleLoginStatus.loggedIn) {
+                                  Get.toNamed('/home');
+                                } else {
+                                  return Future.value();
+                                }
+                              });
+                        }),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
